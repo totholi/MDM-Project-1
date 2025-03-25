aa74dc3f0d706b30865eeaa9f890d319c080f686
brolivertoth <oliver.totho@gmail.com>
2025-03-25 13:44:28 +0100

    Minor updates

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from flask.helpers import send_file
 app = Flask(__name__)
 
 @app.route("/")
+def initialPage():
+    return send_file("index.html")
+
+@app.route("/index")
 def indexPage():
     return send_file("index.html")
 
